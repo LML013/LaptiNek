1503d755588858b75a09c7439561982b7a5aad7c
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2017-04-13 13:04:28 -0700

    Chapter 2 Questions Done
    Completed chapter 2 questions

---
+++ Automate the Boring Stuff by Al Sweigart/Chapter 2/Ch2Q12.py
@@ -0,0 +1,11 @@
+# Q: 12. What is the difference between range(10), range(0, 10), and range(0, 10, 1) in a for loop?
+
+print("for loop range(10) result")
+for i in range(10):
+	print(i)
+print("for loop range(0,10) result")
+for i in range(0,10):
+	print(i)
+print("for loop range(0,10,1) result")
+for i in range(0,10,1):
+	print(i)
