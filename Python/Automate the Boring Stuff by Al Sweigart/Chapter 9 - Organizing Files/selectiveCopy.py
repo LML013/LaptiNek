75a764ea7b37208c4d4f8369d539898e99a0dd9e
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2018-01-10 16:36:29 -0700

    Worked on selectiveCopy.py
    title

--- Python/Automate the Boring Stuff by Al Sweigart/Chapter 9 - Organizing Files/selectiveCopy.py
+++ Python/Automate the Boring Stuff by Al Sweigart/Chapter 9 - Organizing Files/selectiveCopy.py
@@ -5,10 +5,18 @@
 # will need shutil for copy functions, os for path and walk functions, re for identifying extensions
 import shutil, os, re
 
+def selectiveCopy(folder, ext)  # Specify the file extension and what folder is to be searched
+    folder = os.path.abspath(folder)
+    print("Searching files in %s for files with the %s extension..." % (folder, ext))
 
-def selectiveCopy(foldername, ext)  # Specify the file extension and what folder is to be searched
-    print("Searching files in %s for files with the %s extension..." % (foldername, ext))
+    searchHit = re.compile(r".*" + re.escape(ext))
 
 # TODO: Walk specified folder tree, find files with the specified extension ignore files without using if & re, copy correct files to new folder,
+    for foldername, subfolders, filenames in os.walk(folder):
+
+
+        for filename in filenames:
+            if filename
+            print("Copied %s," % (filename))
 
     print("Done.")
