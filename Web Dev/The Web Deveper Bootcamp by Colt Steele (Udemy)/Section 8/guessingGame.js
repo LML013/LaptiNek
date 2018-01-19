91daddeb21566f2a7643f0085a321a48486ec44e
Luis Miguel Luna <LuisMiguelLuna@gmail.com>
2018-01-19 03:25:03 -0700

    Workin Late
    Worked on Section 9 TWDB
    Played with Personal Website

---
+++ Web Dev/The Web Deveper Bootcamp by Colt Steele (Udemy)/Section 8/guessingGame.js
@@ -0,0 +1,22 @@
+//create secret number
+var secretNumber = 4;
+
+//ask user for guess
+var guess = prompt("Guess a number:");
+guess = Number(guess);
+
+//check if guess is right
+if(guess === secretNumber) {
+	alert("You got it right!");
+}
+//otherwise, check if higher
+else if(guess > secretNumber) {
+	alert("Too high, guess again.");
+}
+
+
+//otherwise, check if lower
+else {
+	alert("Too low, guess again.");
+};
+
